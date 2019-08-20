d59ed67199bb99bb50a1d8d0014a762dd876a519
Payten Giles <payten.giles@gmail.com>
2019-08-20 15:03:01 +1000

    Fix setup_database

--- app/util/migration_runner.rb
+++ app/util/migration_runner.rb
@@ -1,6 +1,7 @@
 require_relative '../common/bootstrap'
 
 Sequel.connect(AppConfig[:db_url]) do |db|
-  Sequel::Migrator.apply(db, QSAPublic.base_dir('migrations'))
+  migrations_dir = File.join(File.dirname(__FILE__), "..", 'migrations')
+  Sequel::Migrator.apply(db, migrations_dir)
 end
 
