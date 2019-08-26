a9c68ff4aadbc8ed16cc147c89dd332a9601e451
Payten Giles <payten.giles@gmail.com>
2019-08-26 10:20:21 +1000

    Index feed migration

--- app/migrations/0001_base.rb
+++ app/migrations/0001_base.rb
@@ -1,4 +1,21 @@
 Sequel.migration do
   up do
+    create_table(:index_feed) do
+      primary_key :id
+
+      Integer :record_id, :null => false
+      Integer :repo_id, :null => false
+      String :record_type, :null => false
+      String :record_uri, :size => 64, :null => false, :unique => true
+      File :blob, :size => :medium, :null => false
+      Integer :system_mtime, :null => false, :default => 0
+    end
+
+    create_table(:index_feed_deletes) do
+      primary_key :id
+
+      String :record_uri, :size => 64, :null => false, :unique => true
+    end
+
   end
 end
