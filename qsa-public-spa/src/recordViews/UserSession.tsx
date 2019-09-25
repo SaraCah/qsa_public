55b3b8781e8a1e43bc1bae2f783bc96b3f455d4e
Payten Giles <payten.giles@gmail.com>
2019-09-25 15:48:28 +1000

    Show error message on login fail and redirect to home on success

--- qsa-public-spa/src/recordViews/UserSession.tsx
+++ qsa-public-spa/src/recordViews/UserSession.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Link} from "react-router-dom";
+import {Link, Redirect} from "react-router-dom";
 import Layout from "./Layout";
 import {RecordContext} from "./Helpers";
 import {Http} from "../utils/http";
@@ -33,16 +33,23 @@ export const LoginPage: React.FC<any> = (route: any) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showLoginFailed, setShowLoginFailed] = useState(false);
+    const [needsRedirect, setNeedsRedirect] = useState('');
 
     const onSubmit = (e: any, appContext: any) => {
+        setShowLoginFailed(false);
         Http.get().login(email, password).then((login_response: any) => {
             if (login_response.authenticated) {
                 appContext.setSessionId(login_response.session_id);
+                setNeedsRedirect('/')
             } else {
                 appContext.setUser(null);
+                setShowLoginFailed(true);
             }
-            console.log(login_response);
         })
+    }
+
+    if (needsRedirect) {
+        return <Redirect to={ needsRedirect } push={ true } ></Redirect>;
     }
 
     return (
@@ -55,6 +62,11 @@ export const LoginPage: React.FC<any> = (route: any) => {
                         <h1>Login</h1>
                         <form method="GET" onSubmit={ (e) => { e.preventDefault(); onSubmit(e, context) } } className="form-inline">
                             <div className="qg-call-out-box">
+                                { showLoginFailed &&
+                                    <div className="alert alert-warning" role="alert" style={ {marginBottom: 20} }>
+                                        <p>Invalid email or password.</p>
+                                    </div>
+                                }
                                 <div className="form-row">
                                     <div className="form-group col-xs-12 col-sm-6">
                                         <label htmlFor="email"
