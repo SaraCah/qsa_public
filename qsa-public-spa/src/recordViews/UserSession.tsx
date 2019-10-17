a036c9184ea1745771d62861c40a171a863e03cd
Payten Giles <payten.giles@gmail.com>
2019-10-17 16:53:20 +1100

    Fix refreshing logout page throwing an error

--- qsa-public-spa/src/recordViews/UserSession.tsx
+++ qsa-public-spa/src/recordViews/UserSession.tsx
@@ -161,7 +161,7 @@ export const LogoutPage: React.FC<any> = (route: any) => {
   const [logoutTriggered, setLogoutTriggered] = useState(false);
 
   const logout = (context: any) => {
-    if (!logoutTriggered) {
+    if (!logoutTriggered && context.sessionId) {
       setLogoutTriggered(true);
       Http.get().logout();
       context.clearSession();
