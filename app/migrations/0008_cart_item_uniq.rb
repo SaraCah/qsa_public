4b1f772672d0fc20fb0c93de324d831f296668cf
Mark Triggs <mark@dishevelled.net>
2019-10-21 11:22:15 +1100

    Need to drop the foreign key before we can clear the unique index

--- app/migrations/0008_cart_item_uniq.rb
+++ app/migrations/0008_cart_item_uniq.rb
@@ -4,12 +4,16 @@ Sequel.migration do
     self[:cart_item].delete
 
     alter_table(:cart_item) do
+      drop_foreign_key(:user_id)
+
       # drop the existing constraint
       begin
         drop_constraint(:user_id)
       rescue
         # not there, all good
       end
+
+      add_foreign_key(:user_id, :user)
 
       # add a uniq_hash column
       add_column(:uniq_hash, String, null: false, unique: true, size: 32)
