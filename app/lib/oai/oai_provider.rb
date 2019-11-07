c85a88097156210315ed11b5f9b094c074401949
Mark Triggs <mark@dishevelled.net>
2019-11-07 13:22:55 +1100

    Add support for deletes

--- app/lib/oai/oai_provider.rb
+++ app/lib/oai/oai_provider.rb
@@ -6,6 +6,13 @@ Templates.define(:oai_identify_response, [:params, :now, :earliest_date_timestam
 Templates.define(:oai_list_identifiers_response, [:params, :now, :identifiers, :next_resumption_token?], "lib/oai/templates/list_identifiers.xml.erb")
 
 OAI_RECORD_TYPES = ['resource', 'archival_object']
+
+OAI_RECORD_TYPE_URI_PATTERNS = {
+  'resource' => '%/resources/%',
+  'archival_object' => '%/archival_objects/%',
+}
+
+
 OAI_REQUESTS_PER_PAGE = 100
 
 class OAIProvider
@@ -22,6 +29,7 @@ class OAIProvider
       raise "Unrecognised OAI verb: #{params[:verb]}"
     end
   end
+
 
   def self.handle_identify(request)
     response = Search.solr_handle_search(
@@ -50,7 +58,8 @@ class OAIProvider
     ]
   end
 
-  def self.handle_list_identifiers(request)
+
+  def self.list_record_identifiers(request)
     response = Search.solr_handle_search(
       'q' => '*:*',
       'fq' => [
@@ -63,10 +72,50 @@ class OAIProvider
       'fl' => 'uri,last_modified_time'
     )
 
-    identifiers = response.fetch('response').fetch('docs')
+    response.fetch('response').fetch('docs')
+
+  end
+
+  def self.list_delete_identifiers(request)
+    DB.open do |db|
+      query = db[:index_feed_deletes]
+      query = request.add_sequel_date_range_filter(query)
+      query = query.from_self.filter(1 => 0)
+
+      # Limit to the record types we care about
+      OAI_RECORD_TYPES.each do |type|
+        query = query.or(Sequel.like(:record_uri, OAI_RECORD_TYPE_URI_PATTERNS.fetch(type)))
+      end
+
+      query = query.order(Sequel.asc(:system_mtime))
+                .limit(OAI_REQUESTS_PER_PAGE + 1,
+                       request.offset)
+                .select(:record_uri, :system_mtime)
+
+      # query.sql
+      require 'pp';$stderr.puts("\n*** DEBUG #{(Time.now.to_f * 1000).to_i} [oai_provider.rb:95 FancyGamefowl]: " + {%Q^query.sql^ => query.sql}.pretty_inspect + "\n")
+
+      query.map {|row|
+        {
+          'uri' => row[:record_uri],
+          'last_modified_time' => Time.at(row[:system_mtime]).utc.iso8601,
+          'deleted' => true,
+        }
+      }
+    end
+  end
+
+  def self.handle_list_identifiers(request)
+    identifiers = if request.state == 'records'
+                    list_record_identifiers(request)
+                  elsif request.state == 'deletes'
+                    list_delete_identifiers(request)
+                  end
 
     next_resumption_token = if identifiers.length > OAI_REQUESTS_PER_PAGE
                               request.next_resumption_token(OAI_REQUESTS_PER_PAGE)
+                            elsif request.state == 'records'
+                              request.resumption_token_for_deletes
                             end
 
     [
@@ -82,11 +131,16 @@ class OAIProvider
 
 
   class OAIRequest
-    attr_reader :verb, :from, :until, :offset, :params
+    RECORDS_STATE = 'records'
+    DELETES_STATE = 'deletes'
+
+    attr_reader :verb, :from, :until, :offset, :params, :state
 
     def initialize(params)
       @verb = params.fetch(:verb)
       @params = params
+
+      @state = RECORDS_STATE
 
       @from = params.fetch(:from, nil)
       @until = params.fetch(:until, nil)
@@ -100,6 +154,20 @@ class OAIProvider
       "last_modified_time:[%s TO %s]" % [time_parse(@from) || '*', time_parse(@until) || '*']
     end
 
+    def add_sequel_date_range_filter(base_ds)
+      if @from
+        time = Time.parse(@from).to_i
+        base_ds = base_ds.where { system_mtime >= time }
+      end
+
+      if @until
+        time = Time.parse(@until).to_i
+        base_ds = base_ds.where { system_mtime <= time }
+      end
+
+      base_ds
+    end
+
     def next_resumption_token(increment)
       Base64::strict_encode64(
         {
@@ -107,6 +175,19 @@ class OAIProvider
           'from' => @from,
           'until' => @until,
           'offset' => @offset + increment,
+          'state' => @state,
+        }.to_json
+      )
+    end
+
+    def resumption_token_for_deletes
+      Base64::strict_encode64(
+        {
+          'nonce' => SecureRandom.hex,
+          'from' => @from,
+          'until' => @until,
+          'offset' => 0,
+          'state' => DELETES_STATE,
         }.to_json
       )
     end
@@ -131,6 +212,7 @@ class OAIProvider
       @from = values.fetch('from', @from)
       @until = values.fetch('until', @until)
       @offset = values.fetch('offset', 0)
+      @state = values.fetch('state', RECORDS_STATE)
     end
   end
 end
