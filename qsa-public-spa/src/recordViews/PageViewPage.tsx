50c8ce503c3014838e52288eeddf34afda180d3d
Mark Triggs <mark@dishevelled.net>
2019-11-14 15:35:17 +1100

    Preview & raw HTML support

--- qsa-public-spa/src/recordViews/PageViewPage.tsx
+++ qsa-public-spa/src/recordViews/PageViewPage.tsx
@@ -7,21 +7,60 @@ export const PageSnippet: React.FC<any> = (props: any) => {
   const [content, setContent] = useState('');
 
   useEffect(() => {
-    Http.get().getPageContent(props.slug)
-        .then((content: string) => {
-          setContent(content);
-          if (props.successCallback) {
-            props.successCallback();
-          }
-        })
-        .catch((error: any) => {
-          if (props.notFoundCallback) {
-            props.notFoundCallback();
-          }
-        });
+    if (props.slug) {
+      Http.get().getPageContent(props.slug)
+          .then((content: string) => {
+            setContent(content);
+            if (props.successCallback) {
+              props.successCallback();
+            }
+          })
+          .catch((error: any) => {
+            if (props.notFoundCallback) {
+              props.notFoundCallback();
+            }
+          });
+    }
   }, [props]);
 
-  return <div dangerouslySetInnerHTML={{__html: content}} />
+  useEffect(() => {
+    const maxIterations = 1000;
+
+    for (let i = 0; i < maxIterations; i++) {
+      const codeBlock = document.querySelector('.qsa-snippet-text p > code');
+
+      if (!codeBlock) {
+        break;
+      }
+
+      let combinedHTML = (codeBlock as HTMLElement).innerText;
+
+      // If there are other adjacent <p><code> blocks following this one, combined them into one chunk.
+      let adjacentBlock = codeBlock.parentNode && codeBlock.parentNode.nextSibling as HTMLElement;
+
+      while (adjacentBlock &&
+        adjacentBlock.childElementCount === 1 &&
+        adjacentBlock.firstChild &&
+        (adjacentBlock.firstChild as HTMLElement).tagName === 'CODE') {
+        const nextAdjacentBlock = adjacentBlock.nextSibling;
+
+        combinedHTML += (adjacentBlock.firstChild as HTMLElement).innerText;
+
+        if (adjacentBlock.parentNode) {
+          adjacentBlock.parentNode.removeChild(adjacentBlock);
+        }
+
+        adjacentBlock = nextAdjacentBlock as HTMLElement;
+      }
+
+      // Once we reach the end of our run, insert our code.
+      if (codeBlock.parentNode) {
+        (codeBlock.parentNode as HTMLElement).outerHTML = combinedHTML;
+      }
+    }
+  });
+
+  return <div className="qsa-snippet-text" dangerouslySetInnerHTML={{__html: (props.content || content)}} />
 }
 
 const PageViewPage: React.FC<any> = (route: any) => {
--- qsa-public-spa/src/recordViews/RichText.tsx
+++ qsa-public-spa/src/recordViews/RichText.tsx
@@ -1,8 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import Quill from 'quill';
+import {PageSnippet} from './PageViewPage';
 
 export const RichText: React.FC<any> = (props: any) => {
   const [eltId] = useState('richtext_' + Math.floor(Math.random() * 1000000));
+  const [html, setHTML] = useState(props.value);
+  const [quill, setQuill]: [any, any] = useState(undefined);
+
+  const [editMode, _setEditMode] = useState(true);
+
+  const setEditMode = (value: boolean) => {
+    _setEditMode(value);
+
+    if (!quill) {
+      return;
+    }
+
+    if (value) {
+      quill.root.parentNode.previousSibling.style.display = 'block';
+    } else {
+      quill.root.parentNode.previousSibling.style.display = 'none';
+    }
+  };
 
   let onChange = props.onChange;
   if (!onChange) {
@@ -21,6 +40,7 @@ export const RichText: React.FC<any> = (props: any) => {
                                             [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
                                             ['link'],
                                             ['clean'],
+                                            ['code'],
                                           ],
                                         }
                                       }
@@ -29,13 +49,22 @@ export const RichText: React.FC<any> = (props: any) => {
     quillInstance.clipboard.dangerouslyPasteHTML(0, props.value, 'api');
 
     quillInstance.on('text-change', function () {
-      onChange(quillInstance.root.innerHTML || '');
+      const html = quillInstance.root.innerHTML || '';
+      setHTML(html);
+      onChange(html);
     });
+
+    setQuill(quillInstance);
   },
   // eslint-disable-next-line react-hooks/exhaustive-deps 
   []);
 
   return <div>
-    <div id={eltId}></div>
+    <button className="btn-xs" onClick={(e: any) => { e.preventDefault(); setEditMode(!editMode) }}>Toggle Preview</button>
+    <div style={{display: editMode ? 'block' : 'none'}} id={eltId}></div>
+    <div style={{display: editMode ? 'none' : 'block', border: "solid 1px #ccc", padding: '1em'}}>
+      <PageSnippet content={html} />
+    </div>
   </div>;
 }
+
