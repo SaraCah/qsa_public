b9501ca2dbe14c127161083d850f7a3772c796dd
sara.cave1 <sara.cave@gaiaresources.com.au>
2020-02-12 11:14:52 +1000

    took out the db line I didn't need

--- config/config.rb
+++ config/config.rb
@@ -1,9 +1,7 @@
 # create database qsa_public character set UTF8mb4 collate utf8mb4_bin;
 # grant all on qsa_public.* to 'qsa'@'localhost' identified by 'qsa123';
 
-AppConfig[:db_url] = "jdbc:mysql://localhost:3306/qsa_public?useUnicode=true&characterEncoding=UTF-8&user=qsa&password=qsa123"
-AppConfig[:aspace_db_url] = "jdbc:mysql://localhost:3306/qsa_public?useUnicode=true&characterEncoding=UTF-8&user=qsa&password=qsa123"
-AppConfig[:storage_file_path] = "/tmp/qsa_dev"
+# AppConfig[:db_url] = "jdbc:mysql://localhost:3306/qsa_public?useUnicode=true&characterEncoding=UTF-8&user=qsa&password=qsa123"
 # AppConfig[:session_secret] = "randomly_generated_token"
 
 AppConfig[:indexer_interval_seconds] = 5
